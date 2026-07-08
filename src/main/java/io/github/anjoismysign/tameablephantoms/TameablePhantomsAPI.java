6182da425ae408554dbab91c81642505c56ea75a
anjoismysign <lubensaico@gmail.com>
2026-07-08 14:31:13 -0600

    Add Javadoc comments to API and entity interfaces.

--- src/main/java/io/github/anjoismysign/tameablephantoms/TameablePhantomsAPI.java
+++ src/main/java/io/github/anjoismysign/tameablephantoms/TameablePhantomsAPI.java
@@ -7,14 +7,40 @@ import org.bukkit.entity.Phantom;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+/**
+ * Main API interface for the TameablePhantoms plugin.
+ * Provides methods to tame phantoms, check if an entity is a tameable phantom,
+ * and equip them with saddles.
+ */
 public interface TameablePhantomsAPI {
 
+    /**
+     * Checks if the given entity is a tameable phantom.
+     *
+     * @param entity the entity to check
+     * @return the TameablePhantom wrapper if the entity is a tameable phantom, null otherwise
+     */
     @Nullable
     TameablePhantom isTameablePhantom(@NotNull Entity entity);
 
+    /**
+     * Tames a phantom, assigning it to the specified tamer.
+     *
+     * @param phantom the phantom to tame
+     * @param tamer   the owner to assign to the phantom
+     * @return the TameablePhantom wrapper for the newly tamed phantom
+     * @throws IllegalStateException if the phantom is already tamed
+     */
     @NotNull
     TameablePhantom tame(@NotNull Phantom phantom, @NotNull AnimalTamer tamer);
 
+    /**
+     * Sets whether a phantom has a saddle.
+     * A saddled phantom can be ridden by its owner.
+     *
+     * @param phantom the phantom to set the saddle on
+     * @param saddle  true to add a saddle, false to remove it
+     */
     void setSaddle(@NotNull Phantom phantom, boolean saddle);
 
 }
--- src/main/java/io/github/anjoismysign/tameablephantoms/entity/TameablePhantom.java
+++ src/main/java/io/github/anjoismysign/tameablephantoms/entity/TameablePhantom.java
@@ -4,14 +4,33 @@ import org.bukkit.entity.AnimalTamer;
 import org.bukkit.entity.Phantom;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Represents a tamed phantom, wrapping the underlying Bukkit {@link Phantom}
+ * and providing access to its owner and saddle state.
+ */
 public interface TameablePhantom {
 
+    /**
+     * Gets the underlying Bukkit phantom entity.
+     *
+     * @return the phantom entity
+     */
     @NotNull
     Phantom getPhantom();
 
+    /**
+     * Gets the owner (tamer) of this phantom.
+     *
+     * @return the animal tamer who owns this phantom
+     */
     @NotNull
     AnimalTamer getOwner();
 
+    /**
+     * Checks whether this phantom has a saddle equipped.
+     *
+     * @return true if the phantom has a saddle, false otherwise
+     */
     boolean hasSaddle();
 
 }
